dd15f3061240c56c970320a0027a81a0c1eab990
anniekellner <annie.kellner@colostate.edu>
2023-06-20 21:02:46 -0600

    seems to work but would be better using UTMs instead of lat/lon. Getting an error about values.

--- Code/Test_HR.R
+++ Code/Test_HR.R
@@ -8,7 +8,16 @@ library(tidyverse)
 library(ggplot2)
 library(amt)
 library(readr)
+library(terra)
+library(sf)
+library(tmap)
+library(tmaptools)
+library(leaflet)
 library(conflicted) # will show errors if functions used appear in >1 package
+
+conflicts_prefer(
+  dplyr::filter()
+)
 
 rm(list = ls())
 
@@ -39,3 +48,34 @@ cat1$Datetime <- ymd_hms(cat1$Datetime)
 track1 <- make_track(cat1, .x = "Latitude", .y = "Longitude", .t = "Datetime") 
 
 # ERROR: "negative time differences"
+
+negTimes <- cat1 %>%
+  mutate(next_time = lead(Datetime, 1)) %>%
+  mutate(timediff = next_time - Datetime) %>%
+  dplyr::filter(timediff < 0) %>%
+  select(No, UTC_Date, UTC_Time, Datetime, next_time, timediff)
+
+negTimes # For some reason, R interpreted the time for No=1800 as PM when it should have been AM. Do not know why.
+which(cat1$No == 1800) # 1631
+
+cat1[1631,53] <- "2023-02-12 02:00:37"
+
+# Try making track again
+
+track1 <- make_track(cat1, .x = "Latitude [°]", .y = "Longitude [°]", .t = "Datetime", crs = 4326) # 4326 is the EPSG code for lat/long
+
+mcp <- hr_mcp(track1) # default is 95% MCP
+
+mcp_geom <- mcp$mcp$geometry
+
+plot(mcp_geom)
+
+# Quick interactive plot
+
+map <- tm_shape(mcp_geom) +
+  tm_polygons()
+
+lf <- tmap_leaflet(map)
+
+lf
+  
