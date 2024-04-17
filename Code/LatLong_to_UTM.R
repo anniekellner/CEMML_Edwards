457cb072cee2afe40ee2d1da11e797838a488968
Annie Kellner <annie.kellner@colostate.edu>
2024-04-17 13:52:29 -0600

    final

--- Code/LatLong_to_UTM.R
+++ Code/LatLong_to_UTM.R
@@ -7,61 +7,54 @@
 
 library(tidyverse)
 library(sf)
+library(readxl)
 library(here)
-
+library(tmap)
+library(tmaptools)
 
 # ------- DATA  -------------- #
 # sent as .xlsx from Adam Dillon
 
+directory <- here("Data", "Converting Lat_Long to UTMs")
+
+files <- list.files(directory, full.names = TRUE)
+filenames <- list.files(directory, full.names = FALSE)
+filenames <- str_remove_all(filenames, pattern = ".xlsx")
 
 
-gps82328 <- read_excel("Data/Converting Lat_Long to UTMs/GPS_82328_clean_short.xlsx")
-gps82347 <- read_excel("Data/Converting Lat_Long to UTMs/GPS_82347_clean_short.xlsx")
-gps82349 <- read_excel("Data/Converting Lat_Long to UTMs/GPS_82349_clean_short.xlsx")
-gps82354 <- read_excel("Data/Converting Lat_Long to UTMs/GPS_82354_clean_short.xlsx")
-gps84348 <- read_excel("Data/Converting Lat_Long to UTMs/GPS_84348_clean_short.xlsx")
-gps84359 <- read_excel("Data/Converting Lat_Long to UTMs/GPS_84359_clean_short.xlsx")
-gps84303 <- read_excel("Data/Converting Lat_Long to UTMs/GPS_Collar84303_clean.xlsx")
-
-# Create list of dataframes
-
-dfList <- list(gps84303, gps84359, gps84348, gps82354, gps82349, gps82347, gps82328)
-
-# ----- ADD UTM COLUMNS ------------- #
+# ----- IMPORT FILES AND ADD UTM COLUMNS ------------- #
 
 # Convert to spatial object
 
-df <- dfList[[1]]
-
-UTM <- list() # create list to put all dataframes into 
-
-for(i in 1:length(dfList)){
-  df = dfList[i]
+for(i in 1:length(files)){
+  df = read_excel(files[i])
   dfSF = st_as_sf(df, coords = c("Longitude", "Latitude"), crs = st_crs(4326))
   
   dfSF = dfSF %>%
-    cbind(st_coordinates(df)) %>%
+    cbind(st_coordinates(dfSF)) %>%
     rename(c(Longitude = X, Latitude = Y)) 
   
-  dfUTM = dfSF %>%
-    st_transform(dfSF, 32711) %>%
-    cbind(st_coordinates(dfSF)) %>%
-    rename(Easting = X, Northing = Y) %>%
+  dfUTM = st_transform(dfSF, 32711)
+  
+  dfUTM = dfUTM %>%
+    cbind(st_coordinates(dfUTM)) %>%
+    rename(UTM_Easting = X, UTM_Northing = Y) %>%
     st_drop_geometry()
   
-  write_csv(dfUTM, file = )
+  spreadsheet = filenames[i]
+  name = paste0(spreadsheet,"_UTM.csv")
   
-  
+  write_csv(dfUTM, file = here("Results", name))
   
 }
 
-df <- st_as_sf(df, coords = c("Longitude", "Latitude"), crs = st_crs(4326))
+# Test to make sure points look the same
 
-df <- df %>%
-  cbind(st_coordinates(df)) %>%
-  rename(c(Longitude = X, Latitude = Y)) 
+tmap_mode('view')
 
-df2 <- st_transform(df, 32711) %>%
-  cbind(st_coordinates(df2)) %>%
-  rename(Easting = X, Northing = Y) %>%
-  st_drop_geometry()
+tm_shape(dfSF) + 
+  tm_symbols()
+
+tm_shape(dfUTM) + 
+  tm_symbols()
+
